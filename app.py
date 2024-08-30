2e57133dafbfa11828a188445ddc98eaf513b558
Mukit <Kazirafi98@gmail.com>
2024-08-30 07:24:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import numpy as np
 import pickle
@@ -28,35 +29,8 @@ def predict_sentiment(text):
 st.title('Deep RNN Sentiment Analysis App')
 st.write('Enter text to analyze its sentiment.')
 
-# Initialize session state for user_input
-if 'user_input' not in st.session_state:
-    st.session_state.user_input = ""
-
-# Sidebar for example texts
-st.sidebar.title('Example Texts')
-example_1 = "The movie was outstanding, a must-watch!"
-example_2 = "I was disappointed with the service, not coming back."
-example_3 = "Absolutely loved the new restaurant! Great food and atmosphere."
-example_4 = "I feel like the product did not meet my expectations. Very dissatisfied."
-example_5 = "Today’s weather was neither too hot nor too cold; it was just average"
-
-if st.sidebar.button('Load Example 1'):
-    st.session_state.user_input = example_1
-
-if st.sidebar.button('Load Example 2'):
-    st.session_state.user_input = example_2
-
-if st.sidebar.button('Load Example 3'):
-    st.session_state.user_input = example_3
-
-if st.sidebar.button('Load Example 4'):
-    st.session_state.user_input = example_4
-
-if st.sidebar.button('Load Example 5'):
-    st.session_state.user_input = example_5
-
 # Text input
-user_input = st.text_area('Enter your text here:', value=st.session_state.user_input, height=200)
+user_input = st.text_area('Enter your text here:')
 
 # Button to make prediction
 if st.button('Analyze Sentiment'):
@@ -66,7 +40,13 @@ if st.button('Analyze Sentiment'):
     else:
         st.write("Please enter some text.")
 
-# Custom CSS
+# Optional: Display some example texts
+st.sidebar.title('Example Texts')
+if st.sidebar.button('Load Example 1'):
+    st.sidebar.write('The movie was outstanding, a must-watch!')
+if st.sidebar.button('Load Example 2'):
+    st.sidebar.write('I was disappointed with the service, not coming back.')
+
 st.markdown("""
     <style>
         /* Apply background color to the whole page */
@@ -91,20 +71,6 @@ st.markdown("""
             background-color: #87CEEB;
             color: black;
             padding: 10px 0;
-        }
-
-        /* Button styling */
-        .stButton>button {
-            background-color: #2F4F4F;  /* Dark Gray Background */
-            color: white;
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            font-size: 1rem;
-            cursor: pointer;
-        }
-        .stButton>button:hover {
-            background-color: #45a049;  /* Light Green Background on Hover */
         }
 
         /* Footer styling */
